cb37b2f5310788e7d13224b4a19955c5693b0a82
Newton Duarte <newton_@hotmail.com>
2022-03-01 11:56:49 -0300

    feat: use with ssr guest hoc to send user to dashboard if it is already logged in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { parseCookies } from 'nookies';
 import { FormEvent, useContext, useState } from 'react'
 import styles from '../../styles/Home.module.scss'
 import { AuthContext } from '../contexts/AuthContext';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -41,19 +42,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-};
+});
